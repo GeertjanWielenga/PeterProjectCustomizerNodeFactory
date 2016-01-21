9fe7be7b9f9ef84360ebadceead3d627a481ffd7
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-01-21 17:31:28 +0100

    new nodelist

--- src/main/java/org/netbeans/modules/ppcnf/main/PeterNodeFactory.java
+++ src/main/java/org/netbeans/modules/ppcnf/main/PeterNodeFactory.java
@@ -1,20 +1,28 @@
 package org.netbeans.modules.ppcnf.main;
 
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.prefs.PreferenceChangeEvent;
 import java.util.prefs.PreferenceChangeListener;
 import java.util.prefs.Preferences;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.event.ChangeListener;
 import org.netbeans.api.annotations.common.StaticResource;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.spi.project.SubprojectProvider;
 import org.netbeans.spi.project.ui.CustomizerProvider2;
+import org.netbeans.spi.project.ui.LogicalViewProvider;
 import org.netbeans.spi.project.ui.support.NodeFactory;
 import org.netbeans.spi.project.ui.support.NodeFactorySupport;
 import org.netbeans.spi.project.ui.support.NodeList;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
+import org.openide.nodes.FilterNode;
+import org.openide.nodes.Node;
 
 @NodeFactory.Registration(projectType = "org-netbeans-modules-java-j2seproject")
 public class PeterNodeFactory implements NodeFactory, PreferenceChangeListener {
@@ -48,6 +56,36 @@ public class PeterNodeFactory implements NodeFactory, PreferenceChangeListener {
         node.setDisplayName("Double-click to configure");
         node.setIconBaseWithExtension(RED);
         return NodeFactorySupport.fixedNodeList(node);
+    }
+    
+    //Currently unused, but the start of change listener support to add/remove
+    private class PeterNodeList implements NodeList<Project> {
+        private final Project project;
+        public PeterNodeList(Project prjct) {
+            this.project = prjct;
+        }
+        @Override
+        public List<Project> keys() {
+            return Arrays.asList(project);
+        }
+        @Override
+        public Node node(final Project project) {
+            AbstractNode node = new AbstractNode(Children.LEAF);
+            node.setDisplayName("Peter");
+            return node;
+        }
+        @Override
+        public void addChangeListener(ChangeListener cl) {
+        }
+        @Override
+        public void removeChangeListener(ChangeListener cl) {
+        }
+        @Override
+        public void addNotify() {
+        }
+        @Override
+        public void removeNotify() {
+        }
     }
 
     @Override
