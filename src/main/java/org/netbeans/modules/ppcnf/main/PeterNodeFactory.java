c90e381b41c3a732e7a58c50c659402346407e16
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-01-21 16:14:53 +0100

    adding icon to distinguish between configuration states

--- src/main/java/org/netbeans/modules/ppcnf/main/PeterNodeFactory.java
+++ src/main/java/org/netbeans/modules/ppcnf/main/PeterNodeFactory.java
@@ -3,6 +3,7 @@ package org.netbeans.modules.ppcnf.main;
 import java.util.prefs.PreferenceChangeEvent;
 import java.util.prefs.PreferenceChangeListener;
 import java.util.prefs.Preferences;
+import org.netbeans.api.annotations.common.StaticResource;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectUtils;
 import org.netbeans.spi.project.ui.support.NodeFactory;
@@ -10,13 +11,17 @@ import org.netbeans.spi.project.ui.support.NodeFactorySupport;
 import org.netbeans.spi.project.ui.support.NodeList;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
-import org.openide.nodes.Node;
 
 @NodeFactory.Registration(projectType = "org-netbeans-modules-java-j2seproject")
 public class PeterNodeFactory implements NodeFactory, PreferenceChangeListener {
 
-    private Node node;
+    private AbstractNode node;
     private Preferences prefs;
+
+    @StaticResource
+    private static final String GREEN = "org/netbeans/modules/ppcnf/main/green.png";
+    @StaticResource
+    private static final String RED = "org/netbeans/modules/ppcnf/main/red.png";
 
     @Override
     public NodeList createNodes(Project project) {
@@ -24,6 +29,7 @@ public class PeterNodeFactory implements NodeFactory, PreferenceChangeListener {
         prefs.addPreferenceChangeListener(this);
         node = new AbstractNode(Children.LEAF);
         node.setDisplayName("Not Configured");
+        node.setIconBaseWithExtension(RED);
         return NodeFactorySupport.fixedNodeList(node);
     }
 
@@ -32,8 +38,10 @@ public class PeterNodeFactory implements NodeFactory, PreferenceChangeListener {
         if (evt.getKey().equals("configureMyNode")) {
             if ("true".equals(prefs.get("configureMyNode", null))) {
                 node.setDisplayName("Configured");
+                node.setIconBaseWithExtension(GREEN);
             } else {
                 node.setDisplayName("Not configured");
+                node.setIconBaseWithExtension(RED);
             }
         }
     }
